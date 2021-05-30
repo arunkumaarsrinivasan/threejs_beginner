d6c8a18cd1f2868460e052658f45380b98ebf4cd
Arun kumaar Sreenivasan <arunkumaar.srinivasan@gmail.com>
2021-05-30 19:23:28 +0530

    AK30052021

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 
 // Canvas
-const canvas = document.querySelector('.webgl')
+const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = { width: 800, height: 600 }
@@ -10,16 +10,49 @@ const sizes = { width: 800, height: 600 }
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1) // width, height, depth
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+//group
+const group = new THREE.Group()
+group.position.set( 0, -0.5, 0)
+group.scale.set( 1, 2, 1)
+group.rotation.y=1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x=-2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x=2
+group.add(cube3)
+
 
 // Camera
+
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // fov, aspect
-camera.position.z = 2
+camera.position.z = 4
 scene.add(camera)
+
+//camera look at
+// camera.lookAt(mesh.position)
+
+//AxesHelper
+
+const axesHelper= new THREE.AxesHelper()
+scene.add(axesHelper)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas })
@@ -30,12 +63,40 @@ renderer.render(scene, camera)
 
 
 
+//object properties👇
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1) // width, height, depth
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+//object positioning 
+
+// mesh.position.x=0.7;
+// mesh.position.y=-0.6;
+// mesh.position.z=1;
+
+// mesh.position.set(0.7, -0.6, 1)
+
+//object scaling
+// mesh.scale.x=1
+// mesh.scale.y=1
+// mesh.scale.z=2
+
+// mesh.scale.set(2,0.5,0.5)
+
+//object rotating
+// mesh.rotation.x=0
+// mesh.rotation.y= Math.PI * 0.5 
+// mesh.rotation.z=0
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.set(0,1,0)
 
 
 
 
 
-
+//some js sample👇
 
 // import './style.css'
 // import * as THREE from 'three'
