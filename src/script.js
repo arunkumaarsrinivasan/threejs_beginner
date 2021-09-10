08fd45125ef342398c4d1c01b020adb014b75b7c
Arun kumaar Sreenivasan <arunkumaar.srinivasan@gmail.com>
2021-09-10 20:55:48 +0530

    10092021

--- src/script.js
+++ src/script.js
@@ -46,7 +46,7 @@ window.addEventListener('dblclick',()=>
     }
     else
     {
-        canvas.exitFullscreen()
+        canvas.cancelFullscreen()
     }
 })
 
@@ -63,6 +63,18 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene()
 
+// const geometry = new THREE.BufferGeometry()
+
+// const vertex1 = new THREE.Vector3(0,0,0)
+// geometry.vertices.push(vertex1)
+
+// const vertex2 = new THREE.Vector3(0,1,0)
+// geometry.vertices.push(vertex2)
+
+// const vertex3 = new THREE.Vector3(1,0,0)
+// geometry.vertices.push(vertex3)
+
+
 
 //group
 const group = new THREE.Group()
@@ -71,31 +83,48 @@ group.scale.set( 1, 1, 1)
 // group.rotation.y=1
 scene.add(group)
 
+
+// const bufferGeometry = new THREE.BufferGeometry()
+
+// const vertex1 = new THREE.Vector3(0,0,0)
+// bufferGeometry.vertices.push(vertex1)
+
+// const vertex2 = new THREE.Vector3(0,1,0)
+// bufferGeometry.vertices.push(vertex2)
+
+// const vertex3 = new THREE.Vector3(0,0,1)
+// bufferGeometry.vertices.push(vertex3)
+
+// const material = new THREE.MeshBasicMaterial(
+//     {
+//         color: 0xff0000,
+//         wireframe:true
+//     }
+// )
+
+// const mesh = new THREE.Mesh(bufferGeometry, material)
+
+// group.add(mesh)
+
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1,5,5,5),
+    new THREE.TorusGeometry(10,3,16,5),
     new THREE.MeshBasicMaterial({color: 0xff0000})
 )
 group.add(cube1)
 
-// const cube1 = new THREE.Mesh(
-//     new THREE.TorusGeometry(10,3,16,5),
-//     new THREE.MeshBasicMaterial({color: 0xff0000})
-// )
-// group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.TorusGeometry(10,3,16,7),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+// cube2.position.x=-2
+group.add(cube2)
 
-// const cube2 = new THREE.Mesh(
-//     new THREE.TorusGeometry(10,3,16,7),
-//     new THREE.MeshBasicMaterial({color: 0x00ff00})
-// )
-// // cube2.position.x=-2
-// group.add(cube2)
-
-// const cube3 = new THREE.Mesh(
-//     new THREE.TorusGeometry(10,3,16,9),
-//     new THREE.MeshBasicMaterial({color: 0x0000ff})
-// )
-// // cube3.position.x=2
-// group.add(cube3)
+const cube3 = new THREE.Mesh(
+    new THREE.TorusGeometry(10,3,16,9),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+// cube3.position.x=2
+group.add(cube3)
 
 
 // Camera
@@ -126,7 +155,7 @@ const camera = new THREE.PerspectiveCamera(
     ) // fov, aspect
 // camera.position.x = 2
 // camera.position.y = 2
-camera.position.z = 4
+camera.position.z = 20
 camera.lookAt(group.position)
 scene.add(camera)
 
@@ -232,118 +261,6 @@ tick()
 // mesh.rotation.z=0
 // mesh.rotation.reorder('YXZ')
 // mesh.rotation.set(0,1,0)
-
-
-
-
-
-//some js sample👇
-
-// import './style.css'
-// import * as THREE from 'three'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-// import * as dat from 'dat.gui'
-
-// // Debug
-// const gui = new dat.GUI()
-
-// // Canvas
-// const canvas = document.querySelector('canvas.webgl')
-
-// // Scene
-// const scene = new THREE.Scene()
-
-// // Objects
-// const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
-// // Materials
-
-// const material = new THREE.MeshBasicMaterial()
-// material.color = new THREE.Color(0xff0000)
-
-// // Mesh
-// const sphere = new THREE.Mesh(geometry,material)
-// scene.add(sphere)
-
-// // Lights
-
-// const pointLight = new THREE.PointLight(0xffffff, 0.1)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-// scene.add(pointLight)
-
-// /**
-//  * Sizes
-//  */
-// const sizes = {
-//     width: window.innerWidth,
-//     height: window.innerHeight
-// }
-
-// window.addEventListener('resize', () =>
-// {
-//     // Update sizes
-//     sizes.width = window.innerWidth
-//     sizes.height = window.innerHeight
-
-//     // Update camera
-//     camera.aspect = sizes.width / sizes.height
-//     camera.updateProjectionMatrix()
-
-//     // Update renderer
-//     renderer.setSize(sizes.width, sizes.height)
-//     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// })
-
-// /**
-//  * Camera
-//  */
-// // Base camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// camera.position.x = 0
-// camera.position.y = 0
-// camera.position.z = 2
-// scene.add(camera)
-
-// // Controls
-// // const controls = new OrbitControls(camera, canvas)
-// // controls.enableDamping = true
-
-// /**
-//  * Renderer
-//  */
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: canvas
-// })
-// renderer.setSize(sizes.width, sizes.height)
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-// /**
-//  * Animate
-//  */
-
-// const clock = new THREE.Clock()
-
-// const tick = () =>
-// {
-
-//     const elapsedTime = clock.getElapsedTime()
-
-//     // Update objects
-//     sphere.rotation.y = .5 * elapsedTime
-
-//     // Update Orbital Controls
-//     // controls.update()
-
-//     // Render
-//     renderer.render(scene, camera)
-
-//     // Call tick again on the next frame
-//     window.requestAnimationFrame(tick)
-// }
-
-// tick()
 
 
 
